703afb4b4d8629343fb3de6ef7334d1a83287c0e
Polomac <polomac.milan@nsoft.com>
2018-02-18 08:58:24 +0100

    Minor fix

--- src/router/router.js
+++ src/router/router.js
@@ -1,8 +1,5 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import home from '../components/Home';
-import tests from '../components/Tests';
-import contact from '../components/Contact';
 
 Vue.use(VueRouter);
 
@@ -10,22 +7,18 @@ export function createRouter() {
     return new VueRouter({
         mode: 'history',
         routes: [{
-          name: home,
           path: '/',
-          redirect: '/home',
+          component: () => import(/* webpackChunkName: "home" */ '../components/Home.vue'),
         },
         {
-            name: home,
             path: '/home',
             component: () => import(/* webpackChunkName: "home" */ '../components/Home.vue'),
         },
         {
-            name: tests,
             path: '/tests',
             component: () => import(/* webpackChunkName: "tests" */ '../components/Tests.vue'),
         },
         {
-            name: contact,
             path: '/contact',
             component: () => import(/* webpackChunkName: "contact" */ '../components/Contact.vue'),
         }]
